db4441f209f653584fb0e0b9ffbd7d0b7e3cb2a9
Андрей Скляр <andreas.skliar@gmail.com>
2023-08-30 00:27:10 +0300

    fix hw3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,12 @@
 import throttle from 'lodash.throttle';
 
     const form = document.querySelector('.feedback-form');
-    const emailInput = form.querySelector('input[name="email"]');
+    const emailInput = form.querySelector('[type="email"]');
     const messageInput = form.querySelector('textarea[name="message"]');
+    const btnSubmit = form.querySelector('[type="submit"]');
     const LOCALSTORAGE_KEY = 'feedback-form-state';
 
     form.addEventListener("input", throttle(setUserInfo, 500));
-    form.addEventListener('submit', submitForm);
     const savedState = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
      if (savedState) {
@@ -15,21 +15,22 @@ import throttle from 'lodash.throttle';
       }
 
 function setUserInfo() {
-  if (emailInput.value === "" || messageInput.value ==="") {
-    alert("Please fill in all fields!");
-    return
-  }
       const currentState = {
         email: emailInput.value.trim(),
         message: messageInput.value.trim(),
       };
 
-    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentState));
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentState));
 };
 
 
 function submitForm(e) {
   e.preventDefault();
+
+  if (emailInput.value === "" || messageInput.value === "") {
+    alert("Please fill in all fields!");
+    return
+  }
 
   const currentState = {
     email: emailInput.value,
@@ -40,4 +41,4 @@ function submitForm(e) {
   localStorage.removeItem(LOCALSTORAGE_KEY);
   form.reset();
 }
-
+btnSubmit.addEventListener("click", submitForm);
